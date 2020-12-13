952cf9109d982111d4517861e6bfb5c88ceaa22d
Dion Johnson <dionjwa@gmail.com>
2020-12-12 18:08:44 -0800

    updated files

---
+++ app.js
@@ -0,0 +1,20 @@
+console.log ("Hi")
+
+// var weatherTest = "Sunshine is fine!";
+// console.log(weatherTest);
+
+
+
+var promptQuestion = "Do you like sunshine?";
+var response = prompt(promptQuestion);
+console.log (response);
+var reflected;
+ var alertMessage;
+
+ if (response === "Yes") {
+     reflected = "That's right!";
+ } else  {
+    reflected = "Move to Seattle!"
+}
+alert (reflected)
+
