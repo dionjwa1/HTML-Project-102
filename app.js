bb2c3b0d8260c0831fadd153e9504a4382eaf6e9
Dion Johnson <dionjwa@gmail.com>
2020-12-13 16:41:53 -0800

    new notes and files

--- app.js
+++ app.js
@@ -5,16 +5,35 @@ console.log ("Hi")
 
 
 
-var promptQuestion = "Do you like sunshine?";
+var promptQuestion = "Do you like sunshine? Type 1 for yes or 2 for no";
 var response = prompt(promptQuestion);
 console.log (response);
 var reflected;
  var alertMessage;
 
- if (response === "Yes") {
+ if (response === "1") {
      reflected = "That's right!";
  } else  {
     reflected = "Move to Seattle!"
+
 }
 alert (reflected)
 
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+    greeting = 'Good Evening';
+}
+    else if (hourNow >12) {
+        greeting = 'Good afternoon';}
+    else if (hourNow > 0) {
+        greeting = 'Good morning';}
+    else {
+        greeting = 'Welcome';
+ }
+document.write(greeting);
+
+
+Element.style.color = green;
