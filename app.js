ad53039ef0a7f2a8b6d8594d4f8fd78c1417a64e
Dion Johnson <dionjwa@gmail.com>
2020-12-15 23:02:44 -0800

    updated file

--- app.js
+++ app.js
@@ -1,56 +1,115 @@
 console.log ("Hi")
 
-// var weatherTest = "Sunshine is fine!";
-// console.log(weatherTest);
+//Start Functions
 
 
 
-var promptQuestion = "Do you like sunshine? Type 1 for yes or 2 for no";
-var response = prompt(promptQuestion);
-console.log (response);
-var reflected;
- var alertMessage;
-var color;
- if (response === "1") {
-     reflected = "That's right!";
- } else  {
-    reflected = "Move to Seattle!"
-}
-alert (reflected)
-
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
-
-if (hourNow > 18) {
-    greeting = 'Good Evening' +" " ;
-}
-    else if (hourNow >12) {
-        greeting = 'Good afternoon' +" ";}
-    else if (hourNow > 0) {
-        greeting = 'Good morning' +" ";}
-    else {
-        greeting = 'Welcome';
- } document.write(greeting);
-
-var customerName = prompt("Please enter your name", "");
-var greeting = customerName;
-var display;
-if (customerName!= null) {
-    document.write("Welcome" + " "+ customerName);
-}
-
-function setColorBasedOnUserInput(response){
-    console.log ('response', response);
-    if (response === "1"){
-        color = "lightblue";
-    } else if (response === "2"){
-        console.log ('I selected 2')
-        color = "lightgreen";
-    }
+function initialMessage(){
+    var promptQuestion = "Do you like sunshine? Type 1 for yes or 2 for no";
+    var response = prompt(promptQuestion);
+    console.log (response);
+    var reflected; 
     
-function getWeather(Temp){
-    var url - 'https://openweathermap.org/city/5809844';
+    if (response === "1") {
+        reflected = "Excellent Choice!";
+    } else  {
+       reflected = "Move to Seattle!";
+    }
+
+    setColorBasedOnInitialMessage(response);
+    alert (reflected);
+}
+
+    // set color based on user reponse form initialMessage funcions
+function setColorBasedOnInitialMessage(response){
+    var color;
+    if (response === "1"){
+        color = "green";
+    } else if (response === "2"){
+        color = "lightblue";
+    } else {
+        color = "orange"
+    }
+
+    document.getElementById("body-color").style.backgroundColor = color;
+}
+    
+function salutation(){
+    var today = new Date;
+    var hourNow = today.getHours();
+    var greeting;
+
+    if (hourNow > 18) {
+        greeting = 'Good Evening' +" " ;
+    } else if (hourNow >12) {
+        greeting = 'Good afternoon' +" ";
+    } else if (hourNow > 0) {
+        greeting = 'Good morning' +" ";
+    } else {
+        greeting = 'Welcome';
+    } 
+
+    document.getElementById("firstEntry").innerHTML = greeting, customerName;
+}
+
+function visitorName(){
+   var customerName = prompt("Please enter your name", "");
+   console.log (customerName);
+    var name = customerName;
+    var display;
+    if (customerName!= null) {
+        document.write("" + " "+ customerName);
 }
 }
-setColorBasedOnUserInput(response);
+
+
+
+function identry(){
+
+    document.getElementById("firstEntry").innerHTML = visitorName;
+
+}
+
+// Start Calls
+initialMessage();
+
+salutation();
+
+visitorName();
+
+
+
+// var promptQuestion = "Do you like sunshine? Type 1 for yes or 2 for no";
+// var response = prompt(promptQuestion);
+// console.log (response);
+// var reflected;
+// var alertMessage;
+// var color;
+//  if (response === "1") {
+//      reflected = "That's right!";
+//  } else  {
+//     reflected = "Move to Seattle!"
+// }
+// alert (reflected)
+
+
+// var today = new Date();
+// var hourNow = today.getHours();
+// var greeting;
+
+// if (hourNow > 18) {
+//     greeting = 'Good Evening' +" " ;
+// }
+//     else if (hourNow >12) {
+//         greeting = 'Good afternoon' +" ";}
+//     else if (hourNow > 0) {
+//         greeting = 'Good morning' +" ";}
+//     else {
+//         greeting = 'Welcome';
+//  } document.write(greeting);
+
+// var customerName = prompt("Please enter your name", "");
+// var greeting = customerName;
+// var display;
+// if (customerName!= null) {
+//     document.write("Welcome" + " "+ customerName); 
